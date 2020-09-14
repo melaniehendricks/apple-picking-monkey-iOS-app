756c284dda76f5878aae45da5b4f631b2aa1d23c
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2020-09-14 14:06:31 -0700

    Initial Commit

---
+++ apple-picking-monkey-iOS-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apple-picking-monkey-iOS-app
+//
+//  Created by Melanie Hendricks on 9/14/20.
+//  Copyright © 2020 Melanie Hendricks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
