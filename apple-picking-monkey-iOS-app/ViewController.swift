6b4104094e1ea153cdc4e74e6a7a81649bdce881
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2020-09-22 15:51:12 -0700

    set up skeleton: added images and empty directional functions

--- apple-picking-monkey-iOS-app/ViewController.swift
+++ apple-picking-monkey-iOS-app/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monkey: UIImageView!
+    @IBOutlet weak var apple1: UIImageView!
+    @IBOutlet weak var apple2: UIImageView!
+    @IBOutlet weak var apple3: UIImageView!
+    @IBOutlet weak var apple4: UIImageView!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.monkey.image = UIImage(named: "monkey.jpeg")
+        self.apple1.image = UIImage(named: "apple.png")
+        self.apple2.image = UIImage(named: "apple.png")
+        self.apple3.image = UIImage(named: "apple.png")
+        self.apple4.image = UIImage(named: "apple.png")
+        
+        
     }
 
 
+    @IBAction func moveUp(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func moveDown(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func moveLeft(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func moveRight(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
 }
 
